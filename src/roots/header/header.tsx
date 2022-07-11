6cedd47532e96fb671bb0965b3b4973eb2215a4c
a.voronova <a.voronova1@g.nsu.ru>
2022-07-11 11:48:16 +0700

    Lost font style for header

--- src/roots/header/header.tsx
+++ src/roots/header/header.tsx
@@ -5,6 +5,7 @@ import {homePages} from "../../utils/pages";
 
 import headerStyles from "./header.module.css"
 import buttonStyles from "../../styles/ui/button.module.css"
+import commonStyles from "../../styles/common.module.css"
 
 
 export const Header = () => {
@@ -14,7 +15,7 @@ export const Header = () => {
         navigate("/login", {replace: true});
     }
 
-    return <div id="header" className={headerStyles.header}>
+    return <div id="header" className={headerStyles.header + " " + commonStyles.common}>
         <Link id="logo" className={headerStyles.logo} to={"/home"}/>
         <Navigation pages={homePages.slice(2)}/>
         <button className={buttonStyles.button} onClick={navigateLogin}>Войти</button>
